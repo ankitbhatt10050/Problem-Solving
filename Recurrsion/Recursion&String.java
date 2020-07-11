531925f44a5bd2b4d99ea5b6d4477dd737c53c32
ankitbhatt10050 <ankitbhatt10050.ab@gmail.com>
2020-07-11 19:06:51 +0530

    Remove X from String

--- Recurrsion/Recursion&String.java
+++ Recurrsion/Recursion&String.java
@@ -1,8 +1,38 @@
 package Recurrsion;
-
+import java.util.*;
 class shin{
+    public static String removeX(String X,String Z,int i,int c)
+    {
+        // String A="";
+        if(i>X.length()-1)
+        {
+            
+            return Z ;
+        }
+        if(X.charAt(i)=='x')
+        {
+            
+            return removeX(X,Z,i+1);
+        }
+        else
+        {
+            
+            Z=Z+X.charAt(i);
+          return removeX(X, Z, i+1);
+        }
+        
+        
+    }
     public static void main(String[]args)
     {
-        System.out.println("timepass");
+        Scanner s=new Scanner(System.in);
+        String S=s.nextLine();
+        String Z="";
+
+        System.out.println(removeX(S,Z,0));
+    
+
+
     }
+
 }
