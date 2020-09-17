eb29a20f5d11ad2f886262a74d972e102db46070
ankitbhatt10050 <ankitbhatt10050.ab@gmail.com>
2020-09-17 09:05:38 +0530

    intersection between 2 array solved

---
+++ Binary_Search/intersectionof2array.java
@@ -0,0 +1,47 @@
+package Binary_Search;
+import java.util.*;
+public class intersectionof2array {
+    public static boolean fun(int arr[],int x)
+    {
+            int st=0;
+            int ed=arr.length-1;
+             
+            while(st<ed)
+            {
+                int mid=st+(ed-st)/2;
+                if(arr[mid]==x)
+                {
+                    return true;
+                }
+                if(arr[mid]>x)
+                {
+                    ed=mid;
+                }
+                else
+                {
+                    st=mid+1;
+                }
+            }
+            return false;
+    }
+    public static void main(String[]args)
+    {
+        int num1[]=new int[]{1,2,2,1};
+        int num2[]=new int[]{2,2};
+
+        Arrays.sort(num1);
+        Arrays.sort(num2);
+
+        HashSet<Integer>x=new HashSet<Integer>();
+
+        for(int i=0;i<num1.length;i++)
+        {
+            if(fun(num2,num1[i]))
+            {
+                x.add(num1[i]);
+            }
+        }
+
+        System.out.print("the answer is "+x);
+    }
+}
